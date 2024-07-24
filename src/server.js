e73514177f4fccbb2f5795ee88723f0ccc3c7ae1
rkzin <rkzinnine@gmail.com>
2024-07-23 21:11:43 -0300

    .

---
+++ src/server.js
@@ -0,0 +1,20 @@
+// Importando o express e instanciando
+var express = require('express');
+var app = express();
+
+// Configurando o servidor para utilizar o EJS e passar o caminho para as views
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+// Configurando uma pasta de arquivos estáticos
+app.use(express.static('src/public'));
+
+// Rota principal
+app.get('/', function (request, response) {
+  response.render('index');
+});
+
+// Iniciando o servidor
+app.listen(3003, function () {
+  console.log('Servidor iniciado na porta', 3003);
+});

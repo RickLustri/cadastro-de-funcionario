4b4cd0ed171cb77d6d3612af8c5595f66170f4a3
rkzin <rkzinnine@gmail.com>
2024-07-24 19:19:52 -0300

    .

--- src/hot-reload.js
+++ src/hot-reload.js
@@ -1,21 +1,25 @@
 const livereload = require("livereload");
 const chokidar = require("chokidar");
 const path = require("path");
+
 const enableHotReload = (app) => {
   // Crie um servidor livereload que observa mudanças nos diretórios de visualizações e públicos
   const liveReloadServer = livereload.createServer();
   liveReloadServer.watch(path.join(__dirname, "views"));
   liveReloadServer.watch(path.join(__dirname, "public"));
+
   // Middleware para conectar o livereload ao Express
   const connectLivereload = require("connect-livereload");
   app.use(connectLivereload());
+
   // Use chokidar para observar os arquivos e reiniciar o servidor
-  const watcher = chokidar.watch([path.join(__dirname, "views"), path.join(__dirname,
-    "public")]);
+  const watcher = chokidar.watch([path.join(__dirname, "views"), path.join(__dirname, "public")]);
   watcher.on("change", (file) => {
     console.log(`${file} has been changed`);
     liveReloadServer.refresh("/");
   });
+
+  // Ative o livereload quando o servidor estiver pronto
   liveReloadServer.server.once("connection", () => {
     setTimeout(() => {
       liveReloadServer.refresh("/");
